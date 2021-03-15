06e192de4ba3c31ce0f57d581185d6267e7734af
Gibsol <68705476+Gibsol@users.noreply.github.com>
2021-03-15 20:21:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parity_Check
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a + " это четное число");
+            }else
+            {
+                Console.WriteLine(a + " это нечетное число");
+            }
+        }
+    }
+}
