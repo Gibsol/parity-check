3345dbcba8ff8ffbbe7d0e2cf53431350b0f8b5c
Gibsol <68705476+Gibsol@users.noreply.github.com>
2021-03-16 13:16:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pc_eng
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine(a + " this is an even number");
+            }
+            else
+            {
+                Console.WriteLine(a + " this is an odd number");
+            }
+        }
+    }
+}
